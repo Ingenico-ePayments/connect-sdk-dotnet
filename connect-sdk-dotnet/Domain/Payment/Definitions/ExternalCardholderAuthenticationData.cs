01189420f1df8cd8ceeb9ce583abdb8d30a7ecb2
Ingenico ePayments <github@epay.ingenico.com>
2020-04-28 10:25:09 +0200

    Release 3.9.0.

--- connect-sdk-dotnet/Domain/Payment/Definitions/ExternalCardholderAuthenticationData.cs
+++ connect-sdk-dotnet/Domain/Payment/Definitions/ExternalCardholderAuthenticationData.cs
@@ -2,10 +2,16 @@
  * This class was auto-generated from the API references found at
  * https://epayments-api.developer-ingenico.com/s2sapi/v1/
  */
+using System;
+
 namespace Ingenico.Connect.Sdk.Domain.Payment.Definitions
 {
     public class ExternalCardholderAuthenticationData
     {
+        public string AcsTransactionId { get; set; } = null;
+
+        public string AppliedExemption { get; set; } = null;
+
         public string Cavv { get; set; } = null;
 
         public string CavvAlgorithm { get; set; } = null;
@@ -14,8 +20,11 @@ namespace Ingenico.Connect.Sdk.Domain.Payment.Definitions
 
         public int? Eci { get; set; } = null;
 
+        public int? SchemeRiskScore { get; set; } = null;
+
         public string ThreeDSecureVersion { get; set; } = null;
 
+        [ObsoleteAttribute("No replacement")]
         public string ThreeDServerTransactionId { get; set; } = null;
 
         public string ValidationResult { get; set; } = null;
--- connect-sdk-dotnet/Domain/Payment/Definitions/ThreeDSecureResults.cs
+++ connect-sdk-dotnet/Domain/Payment/Definitions/ThreeDSecureResults.cs
@@ -6,11 +6,17 @@ namespace Ingenico.Connect.Sdk.Domain.Payment.Definitions
 {
     public class ThreeDSecureResults
     {
+        public string AcsTransactionId { get; set; } = null;
+
+        public string AppliedExemption { get; set; } = null;
+
         public string Cavv { get; set; } = null;
 
         public string DirectoryServerTransactionId { get; set; } = null;
 
         public string Eci { get; set; } = null;
+
+        public int? SchemeRiskScore { get; set; } = null;
 
         public SdkDataOutput SdkData { get; set; } = null;
 
