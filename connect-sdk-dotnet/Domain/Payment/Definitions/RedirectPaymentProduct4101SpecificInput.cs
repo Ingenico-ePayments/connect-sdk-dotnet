a3a058f5ae2a22bb9ebf7825088b2afc79af7f6d
Ingenico ePayments <github@epay.ingenico.com>
2021-08-03 17:04:34 +0200

    Release 3.24.0.

--- connect-sdk-dotnet/Domain/Payment/Definitions/RedirectPaymentProduct4101SpecificInput.cs
+++ connect-sdk-dotnet/Domain/Payment/Definitions/RedirectPaymentProduct4101SpecificInput.cs
@@ -7,23 +7,18 @@ namespace Ingenico.Connect.Sdk.Domain.Payment.Definitions
     public class RedirectPaymentProduct4101SpecificInput
     {
         /// <summary>
+        /// The merchant name as shown to the customer in some payment applications.
+        /// </summary>
+        public string DisplayName { get; set; } = null;
+
+        /// <summary>
         /// The value of this property must be either or 'vpa', 'QRCode', or 'urlIntent'.
         /// </summary>
         public string IntegrationType { get; set; } = null;
 
         /// <summary>
-        /// The merchant name as shown to the customer in some payment applications.
-        /// </summary>
-        public string MerchantName { get; set; } = null;
-
-        /// <summary>
-        /// Some additional transaction information as shown to the customer in some payment applications.
-        /// </summary>
-        public string TransactionNote { get; set; } = null;
-
-        /// <summary>
         /// The Virtual Payment Address (VPA) of the customer.
         /// </summary>
-        public string Vpa { get; set; } = null;
+        public string VirtualPaymentAddress { get; set; } = null;
     }
 }
--- connect-sdk-dotnet/MetaDataProvider.cs
+++ connect-sdk-dotnet/MetaDataProvider.cs
@@ -96,7 +96,7 @@ namespace Ingenico.Connect.Sdk
             }
         }
 
-        const string SDK_VERSION = "3.23.0";
+        const string SDK_VERSION = "3.24.0";
 
         const string SERVER_META_INFO_HEADER = "X-GCS-ServerMetaInfo";
 
