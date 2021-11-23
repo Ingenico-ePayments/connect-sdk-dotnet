b8039247fb7ed9929241b1fcc7c4f2fa1a4d5a06
Ingenico ePayments <github@epay.ingenico.com>
2021-11-23 13:11:57 +0100

    Release 3.27.0.

--- connect-sdk-dotnet/Domain/Definitions/Card.cs
+++ connect-sdk-dotnet/Domain/Definitions/Card.cs
@@ -10,5 +10,10 @@ namespace Ingenico.Connect.Sdk.Domain.Definitions
         /// Card Verification Value, a 3 or 4 digit code used as an additional security feature for card not present transactions.
         /// </summary>
         public string Cvv { get; set; } = null;
+
+        /// <summary>
+        /// The first 2 digits of the card's PIN code. May be optionally submitted for South Korean cards (paymentProductIds 180, 181, 182, 183, 184, 185 and 186). Submitting this property may improve your authorization rate.
+        /// </summary>
+        public string PartialPin { get; set; } = null;
     }
 }
